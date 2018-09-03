03bbeb34b1613d86e74bd816db48b9826da461c6
Dine <ariadne0288@hotmail.com>
2018-09-03 10:23:45 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function cardValidator(cardNumber) {
+  validateParameter(cardNumber);
+  cardNumber = cardNumber.toString().split('').reverse().map(Number).map(digit).reduce((acum, number) => acum + number);
+  if(cardNumber % 10 === 0) {
+    return true;
+  }
+  return false;
+}
+function validateParameter(param) {
+  Number(param.toString().trim());
+  if (param === '') {
+    throw new Error('Empty input')
+  } else if(isNaN(param)) {
+    throw new TypeError('Accepts only numeric digits');
+  }
+}
+function digit(number, index) {
+  index % 2 !== 0 ? number *= 2 : number;
+  number > 9 ? number -=9 : number;
+  return number;
+}
