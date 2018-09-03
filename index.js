50ff115484cc98d30378485c20d0b45de39a864b
Dine <ariadne0288@hotmail.com>
2018-09-03 15:31:03 -0300

    Add gitignore

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ module.exports.cardValidator = function cardValidator(cardNumber) {
 function validateParameter(param) {
   Number(param.toString().trim());
   if (param === '') {
-    throw new Error('Empty input')
+    throw new Error('Empty input');
   } else if(isNaN(param)) {
     throw new TypeError('Accepts only numeric digits');
   }
-};
+}
 function digit(number, index) {
   index % 2 !== 0 ? number *= 2 : number;
   number > 9 ? number -=9 : number;
   return number;
-};
+}
