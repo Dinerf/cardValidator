75ec8ad3eb012e2502426ede2c4edcef670dcd11
Dine <ariadne0288@hotmail.com>
2018-09-03 17:19:16 -0300

    Exceções solicitadas e testes

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 module.exports.cardValidator = function cardValidator(cardNumber) {
-  validateParameter(cardNumber);
-  cardNumber = cardNumber.toString().split('').reverse().map(Number).map(digit).reduce((acum, number) => acum + number);
+  cardNumber = validateParameter(cardNumber).toString().split('').reverse().map(Number).map(digit).reduce((acum, number) => acum + number);
   if(cardNumber % 10 === 0) {
     return true;
   }
   return false;
 };
 function validateParameter(param) {
-  Number(param.toString().trim());
-  if (param === '') {
+  if(param == null) {
     throw new Error('Empty input');
-  } else if(isNaN(param)) {
+  } else if (typeof(param) !== 'number'){
+    throw new TypeError('Not a number')
+  } else if(param.toString().match(/\D/)) {
     throw new TypeError('Accepts only numeric digits');
+  } else if(param < 10) {
+    throw new RangeError('Card number incomplete')
   }
+  return param;
 }
 function digit(number, index) {
   index % 2 !== 0 ? number *= 2 : number;
--- test/index.spec.js
+++ test/index.spec.js
@@ -6,17 +6,22 @@ describe('index', () => {
   describe('#cardValidator', () => {
     describe('Quando não houver parametro', () => {
       it('Deve lançar Error', () => {
-        expect(() => {index.cardValidator('')}).to.throw(Error, /Empty input/);
+        expect(() => {index.cardValidator()}).to.throw(Error, /Empty input/);
       });
     });
-    describe('Quando o parametro for uma string', () => {
+    describe('Quando o parametro não for um número', () => {
       it('Deve lançar TypeError', () => {
-        expect(() => {index.cardValidator('teste')}).to.throw(TypeError, /Accepts only numeric digits/);
+        expect(() => {index.cardValidator('teste')}).to.throw(TypeError, /Not a number/);
       });
     });
-    describe('Quando o parametro for um cartão válido em string', () => {
-      it('Deve retornar true', () => {
-        expect(index.cardValidator('36490102462661')).to.be.true;
+    describe('Quando o parametro contiver caracteres que não são números', () => {
+      it('Deve lançar TypeError', () => {
+        expect(() => {index.cardValidator(9.9)}).to.throw(TypeError, /Accepts only numeric digits/);
+      });
+    });
+    describe('Quando o parametro contiver apenas 1 digito', () => {
+      it('Deve lançar RangeError', () => {
+        expect(() => {index.cardValidator(9)}).to.throw(RangeError, /Card number incomplete/);
       });
     });
     describe('Quando o parametro for um cartão válido', () => {
@@ -24,14 +29,9 @@ describe('index', () => {
         expect(index.cardValidator(36490102462661)).to.be.true;
       });
     });
-    describe('Quando o parametro for um cartão inválido em string', () => {
-      it('Deve retornar false', () => {
-        expect(index.cardValidator('3')).to.be.false;
-      });
-    });
     describe('Quando o parametro for um cartão inválido', () => {
       it('Deve retornar false', () => {
-        expect(index.cardValidator(3)).to.be.false;
+        expect(index.cardValidator(56783)).to.be.false;
       });
     });
   });
